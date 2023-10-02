038e199751026c9b31722ff2275782693f6cabb9
Soomin Ryu <rsm0932@gmail.com>
2023-10-02 22:33:19 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,18 +1,31 @@
+# 기존 풀이
+# def solution(cards1, cards2, goal):
+#     answer = ''
+#     n1 = 0
+#     n2 = 0
+#     for i in goal:
+#         if i == cards1[n1]:
+#             if n1 < len(cards1)-1:
+#                 n1 += 1
+#         elif i == cards2[n2]:
+#             if n2 < len(cards2)-1:
+#                 n2 += 1
+#         else:
+#             answer = 'No'
+#             break
+#         # print(n1, n2)
+#         answer = 'Yes'
+#     return answer
 
+
+# pop 사용
 def solution(cards1, cards2, goal):
-    answer = ''
-    n1 = 0
-    n2 = 0
     for i in goal:
-        if i == cards1[n1]:
-            if n1 < len(cards1)-1:
-                n1 += 1
-        elif i == cards2[n2]:
-            if n2 < len(cards2)-1:
-                n2 += 1
+        if len(cards1) > 0 and i == cards1[0]:
+            cards1.pop(0)       
+        elif len(cards2) >0 and i == cards2[0]:
+            cards2.pop(0)
         else:
-            answer = 'No'
-            break
-        # print(n1, n2)
-        answer = 'Yes'
-    return answer
+            return "No"
+        # print(cards1, cards2, i)
+    return "Yes"
