e369c14249efe76a3b5918f566ae0472786ec91a
Soomin Ryu <rsm0932@gmail.com>
2023-10-02 22:28:55 +0900

    Update 카드 뭉치.py

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,20 +1,5 @@
 def solution(cards1, cards2, goal):
     answer = ''
-    n = 0
-    for i in goal:
-        if i == cards1[0] and len(cards1)>1:
-            del cards1[0]
-        elif i == cards2[0] and len(cards2)>1:
-            del cards2[0]
-        else:
-            answer = 'No'
-            break
-        print(cards1, cards2)
-        answer = 'Yes'
-    return answer
-
-def solution(cards1, cards2, goal):
-    answer = ''
     n1 = 0
     n2 = 0
     for i in goal:
