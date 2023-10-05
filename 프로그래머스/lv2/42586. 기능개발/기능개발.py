277abf729860d1a411c15b4ec0faab05e68e037a
Soomin Ryu <rsm0932@gmail.com>
2023-10-05 21:42:03 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -2,28 +2,22 @@ import math
 
 def solution(progresses, speeds):
     answer = []
-    percent = [100-i for i in progresses]
+    percent = [100-i for i in progresses] # 남은 작업%
     
-    days = list()
-    
+    days = list() # 작업 기간
     for j in range(len(progresses)):
         days.append(math.ceil(percent[j]/speeds[j]))
     
-    day = 0
-    count = 0
+    day, count = 0, 0
     
     for d in range(len(days)):
         if day < days[d]:
             answer.append(count)
             day = days[d]
             count = 1
-            # print("if", answer, count, day, days[d])
         else:
             count +=1
-            # print("else", answer, count, day, days[d])
         
         if d == len(days) - 1:
             answer.append(count)
             return answer[1:]
-        
-        
